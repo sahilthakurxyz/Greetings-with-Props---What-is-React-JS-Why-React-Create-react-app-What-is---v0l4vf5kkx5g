9a9b300b982942da8310ee94e5ed9159c168aee5
sahilthakurxyz <100870058+sahilthakurxyz@users.noreply.github.com>
2022-07-27 20:04:36 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,19 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="" />
+    <Welcome name="Sahil" />
   )
 }
 
-
+function Welcome(props){
+  let name = props.name;
+  return(
+    <>
+    <h1>Hey {name}</h1>
+    <h2>
+      Welcome to Newton School.
+    </h2>
+    </>
+  );
+}
 export default App;
