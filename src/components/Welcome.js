20ffae8740234cd0db1f456e3548010eb3333123
sahilthakurxyz <100870058+sahilthakurxyz@users.noreply.github.com>
2022-07-28 15:13:17 +0530

    change welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome(props){
     return(
         <div>
             <h1>Hey {name}</h1>
-            <h2>Welcome to Newton School</h2>
+            <h2>Welcome to Newton School.</h2>
         </div>
     );
 }
