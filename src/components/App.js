4c264947053ff4b016c74b326384cfa343fdb0ae
sahilthakurxyz <100870058+sahilthakurxyz@users.noreply.github.com>
2022-09-24 01:53:14 +0530

    h2 location change

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 function App(){
-  return <Welcome name ="Sahil Thakur" />
+  <>
+  return <Welcome name="Sahil Thakur" />
+  <h2>Welcome to Newton School.</h2>
+  </>
 }
 
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,6 @@ function Welcome(props){
     return(
         <>
             <h1> Hey ! {name}</h1>
-            <h2>Welcome to Newton School.</h2>
         </>
     );
 }
