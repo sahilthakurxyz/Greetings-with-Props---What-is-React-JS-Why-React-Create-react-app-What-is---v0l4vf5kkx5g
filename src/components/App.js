a2b2ba1dcce740f394d3a5c676db4e7143f24753
sahilthakurxyz <100870058+sahilthakurxyz@users.noreply.github.com>
2022-09-24 01:59:27 +0530

    savechanges

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 function App(){
-  <>
+ 
   return <Welcome name="Sahil Thakur" />
-  <h2>Welcome to Newton School.</h2>
-  </>
+ 
 }
 
 export default App;
