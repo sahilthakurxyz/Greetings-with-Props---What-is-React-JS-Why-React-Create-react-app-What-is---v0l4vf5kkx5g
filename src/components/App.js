27632eb1ff051dabe4ed3dbe974cb6e360242752
sahilthakurxyz <100870058+sahilthakurxyz@users.noreply.github.com>
2022-09-25 19:28:36 +0530

    changeArrowFun

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
-function App(){
+const App = ()=>{
  let name = "sahil thakur"
-  return <Welcome name={name} />
- 
+  return
+  (
+  <Welcome name={name} />
+  );
 }
 
 export default App;
